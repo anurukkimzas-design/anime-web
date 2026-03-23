05fc72a98bb1bbf4907530b16c6e98be54785183
anurukkimzas-design <anurukkimzas@gmail.com>
2026-03-23 23:19:47 +0700

    Create data.js

---
+++ data.js
@@ -0,0 +1,26 @@
+const animeData = [
+{
+id:1,
+title:"Attack on Titan",
+img:"https://cdn.myanimelist.net/images/anime/1171/109222.jpg",
+desc:"สงครามมนุษย์กับไททัน"
+},
+{
+id:2,
+title:"Jujutsu Kaisen",
+img:"https://cdn.myanimelist.net/images/anime/1764/126627.jpg",
+desc:"โลกของคำสาป"
+},
+{
+id:3,
+title:"Solo Leveling",
+img:"https://cdn.myanimelist.net/images/anime/1337/99013.jpg",
+desc:"ฮันเตอร์ที่แข็งแกร่งที่สุด"
+},
+{
+id:4,
+title:"One Piece",
+img:"https://cdn.myanimelist.net/images/anime/6/73245.jpg",
+desc:"การผจญภัยของลูฟี่"
+}
+];
