1fadb99b063d393b87ac7025ca3832af93afafc8
anurukkimzas-design <anurukkimzas@gmail.com>
2026-03-25 20:40:11 +0700

    Update data.js

--- data.js
+++ data.js
@@ -1,22 +1,5 @@
-const animeList = [
-    {
-        title: "Attack on Titan",
-        image: "https://m.media-amazon.com/images/M/MV5BNDFjYTIxMjctYTQ2ZC00OGQ4LWE3OGYtNDdiMzNiNDEzNDU1XkEyXkFqcGdeQXVyMTAxNzQ1NzI@._V1_.jpg",
-        status: "ซับไทย"
-    },
-    {
-        title: "Jujutsu Kaisen",
-        image: "https://m.media-amazon.com/images/M/MV5BNGYyZGM5MGMtYTY2Ni00M2Y1LWIzNjQtYWMyM2VlMmEzMGE0XkEyXkFqcGdeQXVyMTAxNzQ1NzI@._V1_.jpg",
-        status: "พากย์ไทย"
-    },
-    {
-        title: "One Piece",
-        image: "https://m.media-amazon.com/images/M/MV5BODcwNWE3OTItMDc4ZC00NDY0LThjZWItMjJlYmUxYjkzZWMxXkEyXkFqcGdeQXVyMTAxNzQ1NzI@._V1_.jpg",
-        status: "ซับไทย"
-    },
-    {
-        title: "Solo Leveling",
-        image: "https://m.media-amazon.com/images/M/MV5BODViZGM5NmEtYzhjZS00ZWRhLThiYjItYjE2NGRlZDI2NDFkXkEyXkFqcGdeQXVyMTEzMTI1Mjk3._V1_.jpg",
-        status: "ซับไทย"
-    }
-];
+{
+    title: "Attack on Titan",
+    image: "img/aot.jpg",
+    status: "ซับไทย"
+},
