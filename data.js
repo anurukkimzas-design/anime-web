08d3ab41e75def2ee7cb6637e5153232423eb617
anurukkimzas-design <anurukkimzas@gmail.com>
2026-03-25 20:31:06 +0700

    Update data.js

--- data.js
+++ data.js
@@ -1,22 +1,22 @@
 const animeList = [
     {
         title: "Attack on Titan",
-        image: "https://s4.anilist.co/file/anilistcdn/media/anime/cover/large/bx16498-73pe7m6Z2aNU.jpg",
+        image: "https://m.media-amazon.com/images/M/MV5BNDFjYTIxMjctYTQ2ZC00OGQ4LWE3OGYtNDdiMzNiNDEzNDU1XkEyXkFqcGdeQXVyMTAxNzQ1NzI@._V1_.jpg",
         status: "ซับไทย"
     },
     {
         title: "Jujutsu Kaisen",
-        image: "https://s4.anilist.co/file/anilistcdn/media/anime/cover/large/bx113415-bb90gpuc7S0d.jpg",
+        image: "https://m.media-amazon.com/images/M/MV5BNGYyZGM5MGMtYTY2Ni00M2Y1LWIzNjQtYWMyM2VlMmEzMGE0XkEyXkFqcGdeQXVyMTAxNzQ1NzI@._V1_.jpg",
         status: "พากย์ไทย"
     },
     {
         title: "One Piece",
-        image: "https://s4.anilist.co/file/anilistcdn/media/anime/cover/large/bx21-69Y8S569ZAsr.jpg",
+        image: "https://m.media-amazon.com/images/M/MV5BODcwNWE3OTItMDc4ZC00NDY0LThjZWItMjJlYmUxYjkzZWMxXkEyXkFqcGdeQXVyMTAxNzQ1NzI@._V1_.jpg",
         status: "ซับไทย"
     },
     {
         title: "Solo Leveling",
-        image: "https://s4.anilist.co/file/anilistcdn/media/anime/cover/large/bx151807-m17Z77VnS9vS.jpg",
+        image: "https://m.media-amazon.com/images/M/MV5BODViZGM5NmEtYzhjZS00ZWRhLThiYjItYjE2NGRlZDI2NDFkXkEyXkFqcGdeQXVyMTEzMTI1Mjk3._V1_.jpg",
         status: "ซับไทย"
     }
 ];
