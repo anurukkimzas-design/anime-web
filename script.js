12719a059ee44e153eb873baff4687eda70c4064
anurukkimzas-design <anurukkimzas@gmail.com>
2026-03-25 20:03:47 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,33 @@
-const list = document.getElementById("list");
+const list = document.getElementById("animeList")
 
-if(list){
-animeData.forEach(a=>{
-list.innerHTML+=`
-<div class="card" onclick="openAnime(${a.id})">
-<img src="${a.img}">
-<h3>${a.title}</h3>
+animeData.forEach(anime=>{
+list.innerHTML += `
+<div class="card" onclick="openAnime(${anime.id})">
+<img src="${anime.img}">
+<div class="title">${anime.title}</div>
 </div>
-`;
-});
-}
+`
+})
 
 function openAnime(id){
-window.location="anime.html?id="+id;
+window.location="anime.html?id="+id
 }
+
+const search = document.getElementById("search")
+
+search.addEventListener("input",()=>{
+const value = search.value.toLowerCase()
+
+list.innerHTML=""
+
+animeData
+.filter(a=>a.title.toLowerCase().includes(value))
+.forEach(anime=>{
+list.innerHTML += `
+<div class="card" onclick="openAnime(${anime.id})">
+<img src="${anime.img}">
+<div class="title">${anime.title}</div>
+</div>
+`
+})
+})
