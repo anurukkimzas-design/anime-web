75eadfdabd586c7d6818165248f236f37c3b2ef7
anurukkimzas-design <anurukkimzas@gmail.com>
2026-03-25 20:10:28 +0700

    Update data.js

--- data.js
+++ data.js
@@ -1,26 +1,5 @@
 const animeData = [
-{
-id:1,
-title:"Attack on Titan",
-img:"https://cdn.myanimelist.net/images/anime/10/47347.jpg",
-desc:"สงครามมนุษย์กับไททัน"
-},
-{
-id:2,
-title:"Jujutsu Kaisen",
-img:"https://cdn.myanimelist.net/images/anime/1171/109222.jpg",
-desc:"โลกของคำสาป"
-},
-{
-id:3,
-title:"Solo Leveling",
-img:"https://cdn.myanimelist.net/images/anime/1246/135728.jpg",
-desc:"นักล่าที่อ่อนแอที่สุด"
-},
-{
-id:4,
-title:"One Piece",
-img:"https://cdn.myanimelist.net/images/anime/6/73245.jpg",
-desc:"การผจญภัยของลูฟี่"
-}
-]
+    { title: "Attack on Titan", image: "link-to-image.jpg", status: "พากย์ไทย" },
+    { title: "Jujutsu Kaisen", image: "link-to-image.jpg", status: "ซับไทย" },
+    // เพิ่มเรื่องอื่นๆ ตรงนี้
+];
