c3fcefad1e91f4b9a2713038fac94d19b574c721
anurukkimzas-design <anurukkimzas@gmail.com>
2026-03-25 20:20:09 +0700

    Update data.js

--- data.js
+++ data.js
@@ -1,5 +1,22 @@
-const animeData = [
-    { title: "Attack on Titan", image: "link-to-image.jpg", status: "พากย์ไทย" },
-    { title: "Jujutsu Kaisen", image: "link-to-image.jpg", status: "ซับไทย" },
-    // เพิ่มเรื่องอื่นๆ ตรงนี้
+const animeList = [
+    {
+        title: "Attack on Titan",
+        image: "https://s4.anilist.co/file/anilistcdn/media/anime/cover/large/bx16498-73pe7m6Z2aNU.jpg",
+        status: "ซับไทย"
+    },
+    {
+        title: "Jujutsu Kaisen",
+        image: "https://s4.anilist.co/file/anilistcdn/media/anime/cover/large/bx113415-bb90gpuc7S0d.jpg",
+        status: "พากย์ไทย"
+    },
+    {
+        title: "One Piece",
+        image: "https://s4.anilist.co/file/anilistcdn/media/anime/cover/large/bx21-69Y8S569ZAsr.jpg",
+        status: "ซับไทย"
+    },
+    {
+        title: "Solo Leveling",
+        image: "https://s4.anilist.co/file/anilistcdn/media/anime/cover/large/bx151807-m17Z77VnS9vS.jpg",
+        status: "ซับไทย"
+    }
 ];
